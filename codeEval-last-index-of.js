d0d1365c7089fa81bc606bbd52408301bd0eb153
Saad Siddiq <siddiq.saad@gmail.com>
2022-05-16 07:37:12 -0600

    init commit

---
+++ codeEval-last-index-of.js
@@ -0,0 +1,15 @@
+// Returns last index of given value in array (or -1 if not in array)
+let lastIndexOf = function(array, value) {
+  for (let i = array.length; i >= 0; i--) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
