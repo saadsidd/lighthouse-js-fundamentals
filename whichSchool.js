f09f90439c510d6a8f8edbe761e9493f7802e94a
Saad Siddiq <siddiq.saad@gmail.com>
2022-05-16 05:21:08 -0600

    init commit

---
+++ whichSchool.js
@@ -0,0 +1,10 @@
+// Function to decide which school to attend based on given age
+const whichSchool = function(age) {
+  if (age < 13) {
+    return 'Elementary School';
+  } else if (age >= 13 && age <= 18) {
+    return 'Secondary School';
+  } else {
+    return 'Lighthouse Labs';
+  }
+}
