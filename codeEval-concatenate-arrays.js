f420f4ee75da3f018521fff78b6f0d14ee6e327e
Saad Siddiq <siddiq.saad@gmail.com>
2022-05-16 11:43:30 -0600

    init commit

---
+++ codeEval-concatenate-arrays.js
@@ -0,0 +1,12 @@
+// Add items from array2 to the end of array1
+let concat = function(array1, array2) {
+  for (let item of array2) {
+    array1.push(item);
+  }
+  return array1;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
