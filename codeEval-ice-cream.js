e849914ffafd0eb8fd769e7e04acb425cc4269da
Saad Siddiq <siddiq.saad@gmail.com>
2022-05-16 06:46:00 -0600

    init commit

---
+++ codeEval-ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Add a flavor
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// Get the first flavor
+console.log(iceCreamFlavours[0]);
+
+// Get the last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+// Total number of flavors
+console.log(iceCreamFlavours.length);
