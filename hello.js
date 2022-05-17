c41296f5307ebd44566e08f5a15f9903f2e6438c
Saad Siddiq <siddiq.saad@gmail.com>
2022-05-17 00:38:21 -0600

    added function return example

--- hello.js
+++ hello.js
@@ -5,3 +5,10 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
